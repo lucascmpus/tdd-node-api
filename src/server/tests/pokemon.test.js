a3f85e9a31546db372de0d9f67428b7387e3f46e
camp.us <lucascmpus@gmail.com>
2023-05-07 01:46:32 -0300

    test: find all/one pokemon

--- src/server/tests/pokemon.test.js
+++ src/server/tests/pokemon.test.js
@@ -1,9 +1,22 @@
 const axios = require("axios");
 
+const request = function (url) {
+  return axios.get(`http://localhost:3030/${url}`);
+};
+
+const randomNumberId = function () {
+  return Math.floor(Math.random() * 822) + 1;
+};
+
 test("should return all pokemons", async function () {
-  const response = await axios
-    .get("http://localhost:3030/pokemons")
-    .then((res) => res.data);
+  const response = await request("pokemons").then((res) => res.data);
 
   expect(response).toHaveLength(822);
+});
+
+test("should return one pokemon", async function () {
+  const id = randomNumberId();
+  const response = await request(`pokemon/${id}`).then((res) => res.data);
+
+  expect(response.id).toBe(id);
 });
